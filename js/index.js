68ddef81bbd3b8432ead689eb385899f6731147b
danny <danny@pop-os.localdomain>
2020-06-24 20:09:03 -0400

    remove resume button

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 Array.prototype.forEach.call(document.getElementsByClassName('bg-gradient'), function(e) {
-    if(screen.width > 630 && e.tagName.toLowerCase() === 'body') return;
+    if(screen.availWidth > 630 && e.tagName.toLowerCase() === 'body') return;
     console.log(screen.width + e.tagName)
     let color2 = getColor();
-    let css = 'linear-gradient(45deg, ' + getColor() + ', ' + color2 + ')'
-    console.log(css);
+    let css = 'linear-gradient(' + Math.floor(Math.random() * 360) + 'deg, ' + getColor() + ', ' + getColor() + ', ' + color2 + ')';
+    // let css = 'radial-gradient(circle, ' + getColor() + ' 0%, ' + getColor() + ' 90%, ' + getColor() + ' 95%, ' + getColor() + ' 98%, ' + getColor() + ' 100%);';
+    console.log('' + css);
     e.style.backgroundImage = css;
     e.style.backgroundColor = color2;
     console.log(e.style);
@@ -12,5 +13,5 @@ Array.prototype.forEach.call(document.getElementsByClassName('bg-gradient'), fun
 function getColor(){
     return "hsl(" + 360 * Math.random() + ',' +
         (25 + 70 * Math.random()) + '%,' +
-        (65 + 8 * Math.random()) + '%)'
+        (50 + 10 * Math.random()) + '%)'
 }
