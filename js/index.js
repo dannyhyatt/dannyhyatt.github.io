a420063e6d425fc46aa0b54232ec00e58a07e2d6
danny <danny@pop-os.localdomain>
2020-09-30 13:53:06 -0400

    improve portfolio page

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 Array.prototype.forEach.call(document.getElementsByClassName('bg-gradient'), function(e) {
-    if(screen.availWidth > 630 && e.tagName.toLowerCase() === 'body') return;
+    if(screen.availWidth > 630 && e.tagName.toLowerCase() === 'body' && window.location.pathname === '/') return;
     console.log(screen.width + e.tagName)
     let color2 = getColor();
     let css = 'linear-gradient(' + Math.floor(Math.random() * 360) + 'deg, ' + getColor() + ', ' + getColor() + ', ' + color2 + ')';
@@ -12,6 +12,6 @@ Array.prototype.forEach.call(document.getElementsByClassName('bg-gradient'), fun
 
 function getColor(){
     return "hsl(" + 360 * Math.random() + ',' +
-        (60 + 15 * Math.random()) + '%,' +
-        (65 + 20 * Math.random()) + '%)'
+        (55 + 15 * Math.random()) + '%,' +
+        (70 + 20 * Math.random()) + '%)'
 }
